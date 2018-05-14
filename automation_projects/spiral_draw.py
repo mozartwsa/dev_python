1287e9b187b3f53875e8dc03cce615c156a285b7
Mozart <mozart.almeida@ati.pe.gov.br>
2018-05-14 15:08:38 -0300

    updating spiral draw

--- automation_projects/spiral_draw.py
+++ automation_projects/spiral_draw.py
@@ -2,17 +2,23 @@
 
 import pyautogui, time
 
+pyautogui.FAILSAFE = True
+
 time.sleep(3)
 pyautogui.click()
 
-distance = 2000
+distance = 200
+try:
 
-while distance > 2000:
-    pyautogui.dragRel(distance, 0, duration=0.2) #move right
-    distance = distance - 5
-    pyautogui.dragRel(0, distance, duration=0.2) #move down
-    pyautogui.dragRel(-distance, 0, duration=0.2) #move left
-    distance = distance - 5
-    pyautogui.dragRel(0, -distance, duration=0.2) #move up
+    while distance > 100:
+        pyautogui.dragRel(distance, 0, duration=1) #move right
+        distance = distance - 5
+        pyautogui.dragRel(0, distance, duration=1) #move down
+        pyautogui.dragRel(-distance, 0, duration=1) #move left
+        distance = distance - 5
+        pyautogui.dragRel(0, -distance, duration=1) #move up
+
+except KeyboardInterrupt:
+    print('\nDone.')
 
 
