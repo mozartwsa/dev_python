376efe66dca2fa1e7280ee7f355aff048ddb000d
Mozart <mozart.almeida@ati.pe.gov.br>
2018-05-14 09:55:34 -0300

    changing nudge to press ctrl instead moving the mouse, which not work.

--- automation_projects/nudge_mouse.py
+++ automation_projects/nudge_mouse.py
@@ -1,15 +1,21 @@
 import pyautogui, time
 
+pyautogui.FAILSAFE = False
+
 print('Press Ctrl-C to quit.')
 try:
     while True:
-        x, y = pyautogui.position()
+        #x, y = pyautogui.position()
 
-        time.sleep(30)
+        pyautogui.press('ctrlright')
 
-        pyautogui.moveRel(10, 10, duration=0.2) #ten pixels right/down
-        time.sleep(1)
-        pyautogui.moveRel(-10, -10, duration=0.2) #ten pixels left/up
+        time.sleep(10)
+        #print(str(time.clock()))
+
+        #pyautogui.moveRel(100, 100, duration=0.2) #hundred pixels right/down
+        #time.sleep(1)
+        #pyautogui.moveRel(-100, -100, duration=0.2) #hundred pixels left/up
+        pyautogui.press('ctrlleft')
 
 except KeyboardInterrupt:
     print('\nDone.')
